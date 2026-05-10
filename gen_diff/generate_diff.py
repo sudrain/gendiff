6a903ddf6a5be87f1ca06bb82461d35eb7d65cb6
sudrain <densedra@gmail.com>
2026-05-10 16:04:55 +0500

    add generate_diff function and test

--- gen_diff/generate_diff.py
+++ gen_diff/generate_diff.py
@@ -4,10 +4,12 @@ import json
 def generate_diff(file1, file2):
     data1 = get_json(file1)
     data2 = get_json(file2)
-    keys = set(data1.keys() | data2.keys())
-    # get_diff_line
-    # map(lamda k: get_diff_line(json1, json2), keys)
-    pass
+    all_keys = sorted(set(data1.keys() | data2.keys()))
+    result = list(map(lambda k: get_diff_line(k, data1, data2), all_keys))
+    formated_result = (
+        "{\n" + "\n".join(list(map(lambda x: "\n".join(x), result))) + "\n}"  # type: ignore
+    )
+    return formated_result
 
 
 def get_json(path):
@@ -20,8 +22,8 @@ def get_diff_line(key, dict1, dict2):
     if key in dict1:
         if key in dict2:
             if dict1[key] != dict2[key]:
-                return [f"- {key}: {dict1[key]}", f"+ {key}: {dict2[key]}"]
-            return [f"  {key}: {dict1[key]}"]
-        return [f"- {key}: {dict1[key]}"]
+                return [f" - {key}: {dict1[key]}", f" + {key}: {dict2[key]}"]
+            return [f"   {key}: {dict1[key]}"]
+        return [f" - {key}: {dict1[key]}"]
     if key in dict2 and key not in dict1:
-        return [f"+ {key}: {dict2[key]}"]
+        return [f" + {key}: {dict2[key]}"]
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,4 +1,4 @@
-from gen_diff.generate_diff import get_diff_line, get_json
+from gen_diff.generate_diff import generate_diff, get_diff_line, get_json
 
 
 def test_get_json():
@@ -12,18 +12,18 @@ def test_get_json():
 
 
 def test_get_diff_line():
-    res1 = ["  key: value"]
-    res2 = ["- key: value"]
-    res3 = ["+ key: value"]
-    res4 = ["- key: old", "+ key: new"]
+    res1 = ["   key: value"]
+    res2 = [" - key: value"]
+    res3 = [" + key: value"]
+    res4 = [" - key: old", " + key: new"]
     assert get_diff_line("key", {"key": "value"}, {"key": "value"}) == res1
     assert get_diff_line("key", {"key": "value"}, {"key1": "value"}) == res2
     assert get_diff_line("key", {"key2": "value"}, {"key": "value"}) == res3
     assert get_diff_line("key", {"key": "old"}, {"key": "new"}) == res4
 
 
-# def test_flat_json():
-#     diff = generate_diff("tests/fixtures/file1.json", "tests/fixtures/file2.json")
-#     with open("tests/fixtures/flat_json_result.json") as f:
-#         expected = f.read()
-#     assert diff == expected.strip()  # убираем возможный перенос в конце
+def test_flat_json():
+    diff = generate_diff("tests/fixtures/file1.json", "tests/fixtures/file2.json")
+    with open("tests/fixtures/flat_json_result") as f:
+        expected = f.read()
+    assert diff == expected  # убираем возможный перенос в конце
